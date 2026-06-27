5e8323bc6d8554e6cc4a623b9cd837c224a3d5a4
Palak-Bansal910 <palakbansal910@gmail.com>
2026-06-27 10:41:08 +0530

    Fix symbol pattern output formatting in symbol_pattern.cpp

--- Day_2/symbol_pattern.cpp
+++ Day_2/symbol_pattern.cpp
@@ -6,13 +6,13 @@ int main(){
     cout<<endl;
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n - i; j++){
-            cout<<" ";
+            cout<<"";
         }
-        for(int k = 1; k <= 2*i -1; k++){
+        for(int k = 1; k <= 2*i -i; k++){
             if(i % 2 == 0){
-                cout<<"#";
+                cout<<"# ";
             }else{
-                cout<<"@";
+                cout<<"@ ";
             }
         }
         cout<<endl;
