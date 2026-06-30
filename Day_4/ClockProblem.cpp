bac29316afeff2fdc4603b6490f1e129f9ab3375
Palak-Bansal910 <palakbansal910@gmail.com>
2026-06-30 06:05:36 +0530

    Refactor findAngle function for improved readability and structure

--- Day_4/ClockProblem.cpp
+++ Day_4/ClockProblem.cpp
@@ -6,9 +6,20 @@ int findAngle(int hour, int minute){
         cout << "Invalid input." << endl;
         return -1;
     }
-    
+    int minuteAngle = 6 * minute;
+    int hourAngle = 30 * hour + (minute / 2);
+    int angle = abs(hourAngle - minuteAngle);
+    if(angle > 180){
+        angle = 360 -angle;
+    }
+    return angle;
 }
 int main(){
-
+    int hour, minute;
+    cout << "Enter the hour: ";
+    cin >> hour;
+    cout << "Enter the minute: ";
+    cin >> minute;
+    cout << "The angle between the hour and minute hands is: " << findAngle(hour, minute) << " degrees." << endl;
     return 0;
 }
