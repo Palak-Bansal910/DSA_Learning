92d1e31fb522c08903267f00e25240031e8fb6e3
Palak-Bansal910 <palakbansal910@gmail.com>
2026-07-07 10:16:34 +0530

    Add runningSum implementation to calculate the running sum of an array

---
+++ Day_11/RunningSum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        vector<int> ans;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
